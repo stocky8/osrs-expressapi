35a3424596ee97a0b74c8fcacdc982ccd0efda18
Hunter Stockdill <stocky88400@gmail.com>
2018-12-11 23:46:35 -0600

    Added functionality for items

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ const osrs = require("osrs-wrapper");
 
 app.get('/osrs.api/stats/:ingameName', function(req, res){
   var plyName = req.params.ingameName;
-  try{
-    osrs.hiscores.getPlayer(plyName).then(player => {
-      var plyStats = JSON.stringify(player, null, 2);
-      console.log("Sending stats for " + plyName);
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(plyStats);
-      return;
-    });
-  }catch(err){
-    console.log("Error retrieving stats for " + plyName);
-    res.status(203).send("Error retrieving stats for " + plyName + "<br>" + err);
-  }
+  osrs.hiscores.getPlayer(plyName).then(player => {
+    var plyStats = JSON.stringify(player, null, 2);
+    console.log("Sending stats for " + plyName);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(plyStats);
+    return;
+  });
 });
 
+app.get('/osrs.api/item/:itemid', function(req, res){
+  // Works for item's Text Name only for now
+  var itemid = req.params.itemid;
+  osrs.ge.getItem(itemid).then(item => {
+    console.log("Sending info for item '" + itemid + "'");
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(item);
+  })
+});
 
 app.get('/', function(req, res){
   res.status(404).send('<h1>404 - Not Found</h1>');
@@ -26,4 +30,4 @@ app.get('/', function(req, res){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-http.timeout = 1000;
+//http.timeout = 5000;
